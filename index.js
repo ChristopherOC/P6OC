41afe079428f23dc01cc57cd6d87df3739d5f342
ChristopherOC <75973858+ChristopherOC@users.noreply.github.com>
2022-04-08 00:24:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const urlbestRatings = "http://localhost:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&format=json&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&rating=&rating_contains=&sort_by=-imdb_score&title=&title_contains=&writer=&writer_contains=&year=&page=1&page_size=7"
-const urlAction = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=sci-fi&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
-const urlSciFi = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=sci-fi&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
-const urlRomance = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=romance&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
-
+const urlAction = "http://localhost:8000/api/v1/titles/?genre=Action&sort_by=votes&page=1&pagesize=7"
+const urlSciFi = "http://localhost:8000/api/v1/titles/?genre=Sci-Fi&sort_by=-votes&page=1&page_size=7"
+const urlRomance = "http://localhost:8000/api/v1/titles/?genre=Romance&sort_by=votes&page=1&pagesize=7"
 
 /**
  * requete
@@ -25,6 +24,9 @@ const urlRomance = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_yea
      const SciFiMovies = await get(urlSciFi)
      const RomanceMovies = await get(urlRomance)
      await loadCarousel("BestMovies", bestRatings)
+     await loadCarousel("ActionMovies", ActionMovies)
+     await loadCarousel("SciFiMovies",SciFiMovies)
+     await loadCarousel("RomanceMovies",RomanceMovies)
     //  console.log("best movies",BestMovies)
     //  console.log("action",ActionMovies)
     //  console.log("scifi", SciFiMovies)
@@ -33,13 +35,16 @@ const urlRomance = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_yea
  }
 
 function loadCarousel(category, movies_data){
+console.log(category)
    let movies_images = document.getElementsByClassName(category)[0].children
-//    console.log(movies_images)
-   for(let i=0; i< movies_images.length; i++){
-       console.log(movie_image)
-       movie_image.src=""
+   console.log(movies_images)
     
-   }
+    movies_data.forEach((movie_data, index)=>{
+        let movie_image = movies_images[index]
+        movie_image.src= movie_data.image_url
+    })
+
+
 
 }
 
