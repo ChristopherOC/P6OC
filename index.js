389505f989e8079601a87cd0c3124f06808917c8
ChristopherOC <75973858+ChristopherOC@users.noreply.github.com>
2022-05-05 10:40:39 +0200

    post session, corriger le scrroll carousel + add element a la modal

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// const urlBestMovie = "http://localhost:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=9&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=2&rating=&rating_contains=&sort_by=votes&title=&title_contains=&writer=&writer_contains=&year="
 const urlbestRatings = "http://localhost:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&format=json&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&rating=&rating_contains=&sort_by=-imdb_score&title=&title_contains=&writer=&writer_contains=&year=&page=1&page_size=7"
-const urlAction = "http://localhost:8000/api/v1/titles/?genre=Action&sort_by=votes&page=1&pagesize=7"
+const urlAction = "http://localhost:8000/api/v1/titles/?genre=Action&sort_by=votes&page=1&page_size=7"
 const urlSciFi = "http://localhost:8000/api/v1/titles/?genre=Sci-Fi&sort_by=-votes&page=1&page_size=7"
-const urlRomance = "http://localhost:8000/api/v1/titles/?genre=Romance&sort_by=votes&page=1&pagesize=7"
+const urlRomance = "http://localhost:8000/api/v1/titles/?genre=Romance&sort_by=votes&page=1&page_size=7"
 
 /**
  * requete
@@ -19,10 +20,12 @@ const urlRomance = "http://localhost:8000/api/v1/titles/?genre=Romance&sort_by=v
 };
 
  async function main(){
+    //  const BestMovie = await get(urlBestMovie)
      const bestRatings = await get(urlbestRatings)
      const ActionMovies = await get(urlAction)
      const SciFiMovies = await get(urlSciFi)
      const RomanceMovies = await get(urlRomance)
+    //  await loadCarousel("BestMovie", BestMovie)
      await loadCarousel("BestMovies", bestRatings)
      await loadCarousel("ActionMovies", ActionMovies)
      await loadCarousel("SciFiMovies",SciFiMovies)
@@ -43,20 +46,21 @@ function loadCarousel(category, movies_data){
         let movie_image = movies_images[index]
         movie_image.src= movie_data.image_url
         movie_image.onclick= function(){
+            toggleModal(movie_data.id)
             console.log(movie_data.id)
+            console.log(movie_data.url)
+            console.log(movie_data.title)
+            console.log(movie_data.imdb_score)
         }
-        // let modal_data = modals_data[index]
-        // modal_data = movie_data.title
     })
 
 }
 
 function previous(category){
-    let movies_images = $("." + category).first().children()
-    console.log(movies_images)
-    let last_movie = movies_images.last()
-    movies_images.remove(last_movie)
-    movies_images.prepend(last_movie)
+    movies_images = document.querySelector("." + category)
+    movies_images.shift()
+    console.log(movies_images.children)
+
     // console.log(movies_images)
     // update_carousel(category)
 }
@@ -89,13 +93,12 @@ function update_carousel(category){
 const modalContainer= document.querySelector(".modal-container")
 const modaltriggers = document.querySelectorAll(".modal-trigger")
 
-modaltriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
+// modaltriggers.forEach(trigger => trigger.addEventListener("click", toggleModal()))
 
 function toggleModal(){
     modalContainer.classList.toggle("active")
-    // document.getElementById("movie__title")
+
+    // document.getElementById("title").innerText = movie_data.title
 }
 
-
 window.addEventListener("DOMContentLoaded", main);
-    
